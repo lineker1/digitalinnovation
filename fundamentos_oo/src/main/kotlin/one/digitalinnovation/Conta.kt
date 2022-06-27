7503b9883238decaf54772cbdf1a77a0f20ab899
Nikao <lineker_sti@hotmail.com>
2022-06-27 19:30:17 -0300

    create clasas account

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/Conta.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val numeroConta: String,
+    val saldo: BigDecimal,
+) {
+    fun deposito(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
