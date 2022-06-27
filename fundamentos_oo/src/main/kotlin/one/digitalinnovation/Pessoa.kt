bb4e005dee7f5e1ff3c2b7f2fd24f5239aa4a850
Nikao <lineker_sti@hotmail.com>
2022-06-27 19:20:04 -0300

    update to apply class member

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/Pessoa.kt
@@ -5,11 +5,12 @@ class Pessoa {
     var cpf: String = "123.123.123-11"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "Nome: $nome \nCpf: $cpf"
     }
 
 fun main(){
     val lineker = Pessoa()
-
-    println(lineker.nome)
-    println(lineker.cpf)
+    println(lineker.pessoaInfo())
 }
