decb07ac7073931924c26062c4726aede39f65ba
Nikao <lineker_sti@hotmail.com>
2022-06-28 20:22:15 -0300

    created and update class, cliente, gerente

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/Gerente.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/Gerente.kt
@@ -3,9 +3,12 @@ package one.digitalinnovation
 class Gerente(
     nome: String,
     cpf: String,
-    salario: Double
-) :Funcionario(nome, cpf, salario){
+    salario: Double,
+    val senha: String
+) :Funcionario(nome, cpf, salario), Logavel{
     override fun calculoAuxilio(): Double = salario * 0.4
+
+    override fun login(): Boolean = "senha123" == senha
     }
 
 
