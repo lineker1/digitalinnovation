e9e048ea3a88c4df34535c2aa68546f9b72719ec
Nikao <lineker_sti@hotmail.com>
2022-06-28 19:41:29 -0300

    created tests and report printer

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/test/TesteFuncionarioAnalista.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/test/TesteFuncionarioAnalista.kt
@@ -1,7 +1,6 @@
 package one.digitalinnovation.test
 
 import one.digitalinnovation.Analista
-import one.digitalinnovation.Funcionario
 
 fun main(){
     val joao = Analista("Joao", "111.111.111-33", 2000.0)
--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/test/TesteFuncionarioGerente.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/test/TesteFuncionarioGerente.kt
@@ -1,8 +1,7 @@
 package one.digitalinnovation.test
 
-import one.digitalinnovation.Analista
-import one.digitalinnovation.Funcionario
 import one.digitalinnovation.Gerente
+
 
 fun main(){
     val maria = Gerente("Maria do Carmo", "222.222.222-44", 4000.0)
