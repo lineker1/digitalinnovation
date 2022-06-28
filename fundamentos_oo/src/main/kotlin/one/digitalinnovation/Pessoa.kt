ff8887a6ca54198ccd38b1277f5a8cbed9d9bcea
Nikao <lineker_sti@hotmail.com>
2022-06-28 19:11:31 -0300

    updated to make person and employer abstract class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/Pessoa.kt
@@ -1,16 +1,6 @@
 package one.digitalinnovation
 
-class Pessoa {
-    var nome: String = "Lineker"
-    var cpf: String = "123.123.123-11"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "Nome: $nome \nCpf: $cpf"
-    }
-
-fun main(){
-    val lineker = Pessoa()
-    println(lineker.pessoaInfo())
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String,
+)
