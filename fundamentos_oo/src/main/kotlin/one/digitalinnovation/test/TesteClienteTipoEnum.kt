06add860ca5f2c7888c838836feba53f4a7a71e3
Nikao <lineker_sti@hotmail.com>
2022-06-28 14:33:51 -0300

    enum class test it,foreach

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/test/TesteClienteTipoEnum.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/test/TesteClienteTipoEnum.kt
@@ -4,9 +4,17 @@ import one.digitalinnovation.ClienteTipo
 
 fun main() {
     ClienteTipo.values().forEach { elemento ->
-        println("${elemento.name} , ${elemento}")
+        println("${elemento.name} , ${elemento.descricao}")
     }
+
+    //usando somente o IT
     ClienteTipo.values().forEach {
-        println("${it.name} , $it")
+        println("${it.name} , ${it.descricao}")
     }
+
+    val pf = ClienteTipo.PF
+    println("${pf.name} , ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name} , ${pj.descricao}")
 }
