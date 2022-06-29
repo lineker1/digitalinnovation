badd047c174149d5dfe113d085278fb555ff834b
Nikao <lineker_sti@hotmail.com>
2022-06-29 16:49:29 -0300

    create int array test class

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/TestIntArray.kt
@@ -0,0 +1,19 @@
+package one.digitalinnovation
+
+fun main() {
+    val values = IntArray(5)
+
+    values[0] = 1
+    values[1] = 2
+    values[2] = 3
+    values[3] = 4
+    values[4] = 5
+
+    for(valor in values){
+        println(valor)
+    }
+
+    values.forEach {
+        println(it)
+    }
+}
